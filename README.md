350b8af964c137aaae57edaff0ae2cc3d17a4aee
Yefri Tavarez <yefritavarez@hotmail.com>
2018-08-17 20:50:44 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Fairweather Innovations
+
+Fairweather Innovations custom app and ERPNext customizations
+
+#### License
+
+General Public License, v3
---
+++ fairweather/config/desktop.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Fairweather Innovations",
+			"color": "#146",
+			"icon": "octicon octicon-flame",
+			"type": "module",
+			"label": _("Fairweather Innovations")
+		}
+	]
