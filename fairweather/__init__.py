89af34a1603cd78254e819bfc53bcb7fc61dca00
Yefri Tavarez <yefritavarez@gmail.com>
2023-01-12 03:30:30 +0000

    Fix report Sales by State

--- fairweather/__init__.py
+++ fairweather/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-__version__ = '1.0.0'
+__version__ = '2.0.0'
 
 from fairweather \
-	.fairweather_innovations \
-	.doctype \
-	.sales_by_state_item_groups \
-	.sales_by_state_item_groups import get_sales_by_state_items_groups
+    .fairweather_innovations \
+    .doctype \
+    .sales_by_state_item_groups \
+    .sales_by_state_item_groups import get_sales_by_state_items_groups
--- fairweather/fairweather_innovations/report/sales_by_state/sales_by_state.py
+++ fairweather/fairweather_innovations/report/sales_by_state/sales_by_state.py
@@ -2,14 +2,14 @@
 # Copyright (c) 2020, Yefri Tavarez and contributors
 # For license information, please see license.txt
 
-from __future__ import unicode_literals
 import frappe
 import fairweather
 
-from frappe import db as database
+from frappe import db
 from frappe import _ as translate
 
-from frappe.utils import flt, today
+from frappe import utils
+from functools import lru_cache
 
 
 class SalesByState:
@@ -27,7 +27,8 @@ class SalesByState:
 
         columns += [cls.get_docfield("Sales Invoice", "Link", "Sales Invoice")]
         columns += [cls.get_docfield("Posting Date", "Date")]
-        columns += [cls.get_docfield("Customer", "Link", "Customer", length=200)]
+        columns += [cls.get_docfield("Customer",
+                                     "Link", "Customer", length=200)]
         columns += [cls.get_docfield("Account", "Link", "Account", length=150)]
 
         columns += [cls.get_docfield("State", "Link", "State")]
@@ -38,7 +39,7 @@ class SalesByState:
         columns += [cls.get_docfield("Freight Amount", "Currency")]
         columns += [cls.get_docfield("Net Total", "Currency")]
 
-        columns += cls.dynamic_cols
+        columns += [d[0] for d in cls.dynamic_cols]
 
         columns += [cls.get_docfield("Total Taxes and Charges", "Currency")]
         columns += [cls.get_docfield("Grand Total", "Currency")]
@@ -50,42 +51,75 @@ class SalesByState:
         return columns
 
     @classmethod
+    def get_tax_amount(cls, charge, row, real):
+        return row.sales_invoice == charge.parent \
+            and charge.description == real
+
+    @classmethod
     def get_data(cls, filters, as_list=True):
         conditions = cls.get_conditions(filters)
 
-        result = cls.get_result_set(filters, conditions)
+        results = cls.get_result_set(filters, conditions)
+        all_charges = cls.get_taxes(filters)
 
-        doctype = "Sales Taxes and Charges"
-        fieldname = "tax_amount"
-        filters = {
-            "parenttype": "Sales Invoice",
-            "parentfield": "taxes",
-        }
+        for row in results:
+            row.freight_amount = cls.get_freight_amount(row)
+            for fancy, real in cls.dynamic_cols:
+                iterator = filter(
+                    lambda charge: cls.get_tax_amount(charge, row, real),
+                    all_charges
+                )
 
-        for d in result:
-            d["freight_amount"] = cls.get_freight_amount(d)
+                tax_amount = sum(
+                    [utils.flt(charge.tax_amount) for charge in iterator]
+                )
 
-            for account in cls.dynamic_cols:
-                filters.update({
-                    "parent": d.sales_invoice,
-                    "description": account,
-                    })
-
-                value = frappe.get_value(doctype, filters, fieldname)
-
-                d[frappe.scrub(account)] = value or 0.000
+                row[frappe.scrub(fancy)] = tax_amount
 
         if as_list:
-            return cls.as_list(result)
+            return cls.as_list(results)
 
-        return result
+        return results
+
+    @classmethod
+    def get_taxes(cls, filters):
+        def _get_conditions():
+            conditions = list()
+
+            conditions += ["invoice.docstatus = 1"]
+
+            if filters.from_date:
+                conditions += ["invoice.posting_date >= %(from_date)s"]
+
+            if filters.to_date:
+                conditions += ["invoice.posting_date <= %(to_date)s"]
+
+            if filters.customer:
+                conditions += ["invoice.customer = %(customer)s"]
+
+            return " And ".join(conditions)
+
+        return db.sql(f"""
+            Select
+                charges.parent,
+                Replace(charges.description, "\n", "") As description,
+                charges.tax_amount
+            From
+                `tabSales Taxes and Charges` As charges,
+                `tabSales Invoice` As invoice
+            Where
+                charges.parenttype = "Sales Invoice"
+                And charges.parentfield = "taxes"
+                And charges.parent = invoice.name
+                And {_get_conditions()}
+        """, filters, as_dict=True)
 
     @classmethod
     def get_result_set(cls, filters, conditions):
         query = cls.get_query(conditions)
         cls.include_item_group_in_filters(filters)
 
-        return database.sql(query, filters, as_dict=True)
+        return db.sql(query, filters, as_dict=True)
 
     @classmethod
     def get_dynamic_cols(cls, filters):
@@ -93,107 +127,106 @@ class SalesByState:
 
         query = """
             Select
-                distinct(
-                    `tabSales Taxes and Charges`.`description`
-                ) As account
+                Distinct(
+                    Replace(charges.description, "\n", "")
+                )
             From
-                `tabSales Taxes and Charges`,
-                `tabSales Invoice`,
-                `tabSales Invoice Item`,
-                `tabAddress`
+                `tabSales Taxes and Charges` As charges,
+                `tabSales Invoice` As invoice,
+                `tabAddress` As address
             Where
-                `tabSales Taxes and Charges`.`parenttype` = "Sales Invoice"
-                And `tabSales Taxes and Charges`.`parentfield` = "taxes"
-                And `tabSales Taxes and Charges`.`parent` = `tabSales Invoice`.`name`
+                charges.parenttype = "Sales Invoice"
+                And charges.parentfield = "taxes"
+                And charges.parent = invoice.name
+                And charges.tax_amount != 0
+                And IfNull(charges.description, "") != ""
                 And {conditions}
         """.format(conditions=conditions)
 
-        return database.sql_list(query, filters)
+        def make_it_fancy(value):
+            return utils.cstr(value) \
+                .replace("State:", "") \
+                .replace("County:", "") \
+                .replace("\t", "") \
+                .strip()
+
+        return [(make_it_fancy(d), d) for d in db.sql_list(query, filters)]
 
     @classmethod
     def include_item_group_in_filters(cls, filters):
         filters.update({
             "item_groups": fairweather.get_sales_by_state_items_groups(),
         })
-        
 
     @classmethod
     def get_query(cls, conditions):
         return """
             Select
-                `tabSales Invoice`.`name` As sales_invoice,
-                `tabSales Invoice`.`posting_date`,
-                `tabSales Invoice`.`customer`,
-                `tabSales Invoice`.`debit_to`,
-                `tabAddress`.`state`,
-                `tabAddress`.`address_line1`,
-                `tabAddress`.`pincode`,
-                `tabAddress`.`city`,
-                `tabAddress`.`address_type`,
-                `tabSales Invoice`.`net_total` As freight_amount,
-                `tabSales Invoice`.`net_total`,
-                `tabSales Invoice`.`total_taxes_and_charges`,
-                `tabSales Invoice`.`grand_total`,
-                `tabSales Invoice`.`outstanding_amount`
+                invoice.name As sales_invoice,
+                invoice.posting_date,
+                invoice.customer,
+                invoice.debit_to,
+                address.state,
+                address.address_line1,
+                address.pincode,
+                address.city,
+                address.address_type,
+                invoice.net_total As freight_amount,
+                invoice.net_total,
+                invoice.total_taxes_and_charges,
+                invoice.grand_total,
+                invoice.outstanding_amount
             From
-                `tabSales Invoice`,
-                `tabSales Invoice Item`,
-                `tabAddress`                   
+                `tabSales Invoice` As invoice,
+                `tabSales Invoice Item` As item,
+                `tabAddress` As address
             Where
                 {conditions}
-                And `tabSales Invoice Item`.`parenttype` = "Sales Invoice"
-                And `tabSales Invoice Item`.`parentfield` = "items"
-                And `tabSales Invoice Item`.`parent` = `tabSales Invoice`.`name`
+                And item.parenttype = "Sales Invoice"
+                And item.parentfield = "items"
+                And item.parent = invoice.name
             Group By
-                `tabSales Invoice`.`name`
+                invoice.name
             Order By
-                `tabSales Invoice`.`name` Asc
+                invoice.name Asc
         """.format(conditions=conditions)
 
     @classmethod
     def get_conditions(cls, filters, ignore_item_groups=False):
         conditions = list()
 
-        conditions += ["`tabSales Invoice`.`docstatus` = 1"]
+        conditions += ["invoice.docstatus = 1"]
 
         if not ignore_item_groups:
-            conditions += ["`tabSales Invoice Item`.`item_group` in %(item_groups)s"]
+            conditions += ["item.item_group in %(item_groups)s"]
 
         if "from_date" in filters:
-            conditions += ["`tabSales Invoice`.`posting_date` >= %(from_date)s"]
+            conditions += ["invoice.posting_date >= %(from_date)s"]
 
         if "to_date" in filters:
-            conditions += ["`tabSales Invoice`.`posting_date` <= %(to_date)s"]
+            conditions += ["invoice.posting_date <= %(to_date)s"]
 
         if "customer" in filters:
-            conditions += ["`tabSales Invoice`.`customer` = %(customer)s"]
+            conditions += ["invoice.customer = %(customer)s"]
 
         if "state" in filters:
-            conditions += ["`tabAddress`.`state` = %(state)s"]
+            conditions += ["address.state = %(state)s"]
 
         address_type = filters.get("address_type")
 
         if address_type == "Billing":
-            conditions += ["`tabSales Invoice`.`customer_address` = `tabAddress`.`name`"]
+            conditions += ["invoice.customer_address = address.name"]
         elif address_type == "Shipping":
-            conditions += ["`tabSales Invoice`.`shipping_address_name` = `tabAddress`.`name`"]
+            conditions += ["invoice.shipping_address_name = address.name"]
         else:
-            conditions += ["`tabAddress`.`name` in (`tabSales Invoice`.`customer_address`, `tabSales Invoice`.`shipping_address_name`)"]
-
+            conditions += [
+                "address.name in (invoice.customer_address, invoice.shipping_address_name)"]
 
         return " And ".join(conditions)
 
     @classmethod
     def get_docfield(cls, label, fieldtype="Data", options="", length=100):
-        kwargs = {
-            "label": translate(label),
-            "fieldtype": fieldtype,
-            "options": options,
-            "length": length,
-        }
-
-        return "{label}:{fieldtype}/{options}:{length}" \
-            .format(**kwargs)
+        return f"{label}:{fieldtype}/{options}:{length}"
 
     @classmethod
     def get_field_list(cls):
@@ -211,14 +244,14 @@ class SalesByState:
             "net_total",
         ]
 
-        fieldlist += [frappe.scrub(col) for col in cls.dynamic_cols]
+        fieldlist += [frappe.scrub(col[0]) for col in cls.dynamic_cols]
 
         fieldlist += [
             "total_taxes_and_charges",
             "grand_total",
             "outstanding_amount",
         ]
-        
+
         return fieldlist
 
     @classmethod
@@ -245,9 +278,9 @@ class SalesByState:
         fieldname = "freight_item"
 
         if not cls.freight_item:
-            cls.freight_item = database \
-                .get_single_value(doctype, 
-                    fieldname, cache=True)
+            cls.freight_item = db \
+                .get_single_value(doctype,
+                                  fieldname, cache=True)
 
         return cls.freight_item
 
@@ -256,7 +289,7 @@ class SalesByState:
         freight_item = cls.get_freight_item()
 
         doctype = "Sales Invoice Item"
- 
+
         filters = {
             "parentfield": "items",
             "parenttype": "Sales Invoice",
@@ -266,11 +299,9 @@ class SalesByState:
 
         fieldname = "net_amount"
 
-
         value = frappe.get_value(doctype, filters, fieldname)
 
         return value or 0.000
-
 
 
 def execute(filters=None):
